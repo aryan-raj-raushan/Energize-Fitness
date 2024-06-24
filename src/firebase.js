9f6ac93f6f6453553eec5473703bf138361e22dc
aryan-raj-raushan <aryanraj.raushan@gmail.com>
2024-06-24 22:25:39 +0530

    env update

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider, OAuthProvider, PhoneAuthProvider } from "f
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAAAfNCdbyCk9iNqUBr0aTCOWdgpEE5G5A",
-  authDomain: "gymate-gym-07.firebaseapp.com",
-  projectId: "gymate-gym-07",
-  storageBucket: "gymate-gym-07.appspot.com",
-  messagingSenderId: "764450117336",
-  appId: "1:764450117336:web:defa1321b2e5f832a9a9ad",
-  measurementId: "G-KZ3WWJ5J54",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,8 +18,3 @@ export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
 export const appleProvider = new OAuthProvider('apple.com');
 export const phoneProvider = new PhoneAuthProvider(auth);
-
-
-// 764450117336-856ihipq5sm7ss2elqbllhp6bb6s604f.apps.googleusercontent.com - client id
-
-// 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 - client secret
